d4a1f40b718a23e6ac5c4f217288c334c4901f81
ZIV LEVY <zivilevy@gmail.com>
2019-12-01 20:13:46 +0200

    Initial Commit

---
+++ testBin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testBin
+//
+//  Created by ZIV LEVY on 01/12/2019.
+//  Copyright © 2019 Yamasee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testBinTests/testBinTests.swift
@@ -0,0 +1,34 @@
+//
+//  testBinTests.swift
+//  testBinTests
+//
+//  Created by ZIV LEVY on 01/12/2019.
+//  Copyright © 2019 Yamasee. All rights reserved.
+//
+
+import XCTest
+@testable import testBin
+
+class testBinTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
